b7276a8743a95cf2879e07ab0a20880a90780f6e
Kieran <maykw002@mymail.unisa.edu.au>
2018-04-09 16:37:38 +0930

    SQUAD Changes
    -Redid quadrants generating for the menu properly.

---
+++ Assets/Sphere-Casting, SQUAD/Scripts/CreateTriangle.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTriangle : MonoBehaviour {
+
+    Mesh mesh;
+    MeshRenderer meshRenderer;
+    Vector3[] vertices;
+    int[] triangles;
+
+    public Material material;
+    private GameObject triangle = null;
+    public GameObject newTriangle;
+
+    // Use this for initialization
+    void Start () {
+        newTriangle = new GameObject();
+        newTriangle.AddComponent<MeshFilter>();
+        meshRenderer = newTriangle.AddComponent<MeshRenderer>();
+
+        meshRenderer.material = material;
+
+        mesh = new Mesh();
+        newTriangle.GetComponent<MeshFilter>().mesh = mesh;
+        vertices = new[] {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,0,0),
+        };
+        mesh.vertices = vertices;
+        triangles = new[] { 0, 1, 2 };
+        mesh.triangles = triangles;
+
+        triangle = Instantiate(newTriangle, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+        triangle.transform.localEulerAngles = new Vector3(0f, 0f, 45f);
+        triangle.transform.SetParent(this.transform, false);
+
+        triangle = Instantiate(newTriangle, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+        triangle.transform.localEulerAngles  = new Vector3(0f, 0f, 135f);
+        triangle.transform.SetParent(this.transform, false);
+
+        triangle = Instantiate(newTriangle, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+        triangle.transform.localEulerAngles = new Vector3(0f, 0f, 225f);
+        triangle.transform.SetParent(this.transform, false);
+
+        triangle = Instantiate(newTriangle, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+        triangle.transform.localEulerAngles = new Vector3(0f, 0f, 315f);
+        triangle.transform.SetParent(this.transform, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
