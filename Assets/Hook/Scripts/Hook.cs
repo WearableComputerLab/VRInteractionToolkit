f4800be66a3374521c5ee42fa51a7c663b19f25a
hanis001 <I_hanan@outlook.com>
2018-04-25 21:07:25 +0930

    Updated Spindle + Wheel

---
+++ Assets/Hook/Scripts/Hook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Information on hook technique
+// http://www.eecs.ucf.edu/isuelab/publications/pubs/Cashion_Jeffrey_A_201412_PhD.pdf pf 13
+
+public class Hook : MonoBehaviour {
+
+    List<GameObject> nearbyObjects;
+
+	// Use this for initialization
+	void Start () {
+        nearbyObjects = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void populateGameObjectList()
+    {
+        var allObjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject each in allObjects)
+        {
+            nearbyObjects.Add(each);
+        }
+    }
+
+}
