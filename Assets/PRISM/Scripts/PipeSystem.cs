b2ca766c358d412375fc494a1a7e3a319a51ca01
hanis001 <I_hanan@outlook.com>
2018-07-01 15:37:02 +0930

    PRISIM test scene

---
+++ Assets/PRISM/Scripts/PipeSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSystem : MonoBehaviour {
+
+	public Pipes pipePrefab;
+
+	public int pipeCount;
+
+	private Pipes[] pipes;
+
+	private void Awake () {
+		pipes = new Pipes[pipeCount];
+		for (int i = 0; i < pipes.Length; i++) {
+			Pipes pipe = pipes[i] = Instantiate<Pipes>(pipePrefab);
+			pipe.transform.SetParent(transform, false);
+			if (i > 0) {
+				pipe.AlignWith(pipes[i - 1]);
+			}
+		}
+	}
+}
---
+++ Assets/PRISM/Scripts/Pipes.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour {
+
+	public Vector3[] points;
+
+	public int pipeRadius = 1;
+	public int pipeSegmentCount = 10;
+	public float ringDistance = 4;
+	public float minCurveRadius, maxCurveRadius;
+	public int minCurveSegmentCount, maxCurveSegmentCount;
+	private float curveRadius;
+	private int curveSegmentCount;
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private int[] triangles;
+	private float curveAngle;
+
+	public void AlignWith (Pipes pipe) {
+		float relativeRotation =
+			Random.Range(0, curveSegmentCount) * 360f / pipeSegmentCount;
+		
+		transform.SetParent(pipe.transform, false);
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.Euler(0f, 0f, -pipe.curveAngle);
+		transform.Translate(0f, pipe.curveRadius, 0f);
+		transform.Rotate(relativeRotation, 0f, 0f);
+		transform.Translate(0f, -curveRadius, 0f);
+		transform.SetParent(pipe.transform.parent);
+	}
+
+	private void Awake () {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Pipe";
+		
+		curveRadius = Random.Range(minCurveRadius, maxCurveRadius);
+		curveSegmentCount =
+			Random.Range(minCurveSegmentCount, maxCurveSegmentCount + 1);
+		
+		SetVertices();
+		SetTriangles();
+		mesh.RecalculateNormals();
+	}
+
+	private void SetVertices () {
+		vertices = new Vector3[pipeSegmentCount * curveSegmentCount * 4];
+		float uStep = ringDistance / curveRadius;
+		curveAngle = uStep * curveSegmentCount * (360f / (2f * Mathf.PI));
+		CreateFirstQuadRing(uStep);
+		int iDelta = pipeSegmentCount * 4;
+		for (int u = 2, i = iDelta; u <= curveSegmentCount; u++, i += iDelta) {
+			CreateQuadRing(u * uStep, i);
+		}
+		mesh.vertices = vertices;
+	}
+
+	private void CreateFirstQuadRing (float u) {
+		float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+		Vector3 vertexA = GetPointOnTorus(0f, 0f);
+		Vector3 vertexB = GetPointOnTorus(u, 0f);
+		for (int v = 1, i = 0; v <= pipeSegmentCount; v++, i += 4) {
+			vertices[i] = vertexA;
+			vertices[i + 1] = vertexA = GetPointOnTorus(0f, v * vStep);
+			vertices[i + 2] = vertexB;
+			vertices[i + 3] = vertexB = GetPointOnTorus(u, v * vStep);
+		}
+	}
+
+	private void CreateQuadRing (float u, int i) {
+		float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+		int ringOffset = pipeSegmentCount * 4;
+		
+		Vector3 vertex = GetPointOnTorus(u, 0f);
+		for (int v = 1; v <= pipeSegmentCount; v++, i += 4) {
+			vertices[i] = vertices[i - ringOffset + 2];
+			vertices[i + 1] = vertices[i - ringOffset + 3];
+			vertices[i + 2] = vertex;
+			vertices[i + 3] = vertex = GetPointOnTorus(u, v * vStep);
+		}
+	}
+
+	private void SetTriangles () {
+		triangles = new int[pipeSegmentCount * curveSegmentCount * 6];
+		for (int t = 0, i = 0; t < triangles.Length; t += 6, i += 4) {
+			triangles[t] = i;
+			triangles[t + 1] = triangles[t + 4] = i + 1;
+			triangles[t + 2] = triangles[t + 3] = i + 2;
+			triangles[t + 5] = i + 3;
+		}
+		mesh.triangles = triangles;
+	}
+
+	private Vector3 GetPointOnTorus (float u, float v) {
+		Vector3 p;
+		float r = (curveRadius + pipeRadius * Mathf.Cos(v));
+		p.x = r * Mathf.Sin(u);
+		p.y = r * Mathf.Cos(u);
+		p.z = pipeRadius * Mathf.Sin(v);
+		return p;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
