3a8fd5e49f76c4f9300191fd2b41413cb7d6e117
Kieran <maykw002@mymail.unisa.edu.au>
2018-10-16 22:32:26 +1030

    Fishing Reel small fix + docker changes
    - Fixed fishing reel bug

--- Assets/Fishing Reel/Scripts/FishingReel.cs
+++ Assets/Fishing Reel/Scripts/FishingReel.cs
@@ -45,7 +45,7 @@ public class FishingReel : MonoBehaviour {
         Vector3 controllerPos = trackedObj.transform.forward;
         if (trackedObj != null) {
             if (controller.GetPressDown(trigger) && pickedUpObject == false) {
-                if (interacionType == InteractionType.Manipulation_Movement && this.GetComponent<SelectionManipulation>().manipulationMovementEnabled == true) {
+                if (interacionType == InteractionType.Manipulation_Movement) {
                     obj.transform.SetParent(trackedObj.transform);
                     extendDistance = Vector3.Distance(controllerPos, obj.transform.position);
                     tempObjectStored = obj; // Storing the object as an instance variable instead of using the obj parameter fixes glitch of it not properly resetting on TriggerUp
