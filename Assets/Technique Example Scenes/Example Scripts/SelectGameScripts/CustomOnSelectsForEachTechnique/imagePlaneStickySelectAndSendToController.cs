bfe8005450b48e65bbc468c811a7ca186cccc20e
hanis001 <I_hanan@outlook.com>
2018-10-28 23:52:12 +1030

    ExampleChanges

---
+++ Assets/Technique Example Scenes/Example Scripts/SelectGameScripts/CustomOnSelectsForEachTechnique/imagePlaneStickySelectAndSendToController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imagePlaneStickySelectAndSendToController : MonoBehaviour {
+
+	public ImagePlane_StickyHand selectObject;
+
+	private TesterController controller;
+
+	private GameObject originalParent;
+
+	// Use this for initialization
+	void Start () {
+		originalParent = this.transform.parent.gameObject;
+		selectObject.selectedObjectEvent.AddListener(tellTesterOfSelection);
+		controller = this.GetComponentInParent<TesterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void tellTesterOfSelection() {
+		print("trying to select");
+		if(selectObject.selectedObject == this.gameObject) {
+			print("success");
+			controller.objectSelected(this.gameObject);
+		}	
+	}
+}
