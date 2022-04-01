a8be1e61c1a0e1b3c28e890039b15d816ce37c6a
Kez <maykw002@mymail.unisa.edu.au>
2022-04-01 18:58:51 +1030

    Updating missing touchpad reference

--- Assets/3DUITK/Techniques/Depth Ray/Scripts/DepthRay.cs
+++ Assets/3DUITK/Techniques/Depth Ray/Scripts/DepthRay.cs
@@ -31,6 +31,7 @@ public class DepthRay : MonoBehaviour {
     public SteamVR_Action_Boolean m_controllerPress;
     private SteamVR_Behaviour_Pose trackedObj;
     public SteamVR_Action_Vector2 m_touchpadAxis;
+    public SteamVR_Action_Vector2 m_touchpad;
 #else
     public GameObject trackedObj;
 #endif
